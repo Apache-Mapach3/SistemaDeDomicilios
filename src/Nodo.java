7b79ff52cd7581408ef33611d38986388311a8b7
Jose <jriverar1@unicartagena.edu.co>
2026-05-20 12:12:06 -0500

    feat: Logica de la entidad  Nodo y Entidad Domicilio

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Nodo {
+    private Object dato;
+    private Nodo izquierda;
+    private Nodo derecha;
+
+    public Nodo() {}
+
+    public Nodo(Object dato) {
+        this.dato = dato;
+    }
+
+    public Object getDato() { return dato; }
+    public void setDato(Object dato) { this.dato = dato; }
+    public Nodo getIzquierda() { return izquierda; }
+    public void setIzquierda(Nodo vecino) { this.izquierda = vecino; }
+    public Nodo getDerecha() { return derecha; }
+    public void setDerecha(Nodo vecino) { this.derecha = vecino; }
 }
